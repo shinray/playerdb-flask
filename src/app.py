34cf1dccc1951d615bbdf1ae9b95e4d45fda7137
skuo <skuo004@ucr.edu>
2024-04-10 17:10:13 -0700

    fix: rename endpoint

--- src/app.py
+++ src/app.py
@@ -5,14 +5,10 @@ app = Flask(__name__)
 
 # TODO: read csv, serve players instead
 employees = [ { 'id': 1, 'name': 'Ashley' }, { 'id': 2, 'name': 'Kate' }, { 'id': 3, 'name': 'Joe' }]
-@app.route('/employees', methods=['GET'])
-def get_employees():
-    return jsonify(employees)
 
-
-# @app.route('/api/players', methods=['GET'])
-# def get_players():
-#  return jsonify()
+@app.route('/api/players', methods=['GET'])
+def get_players():
+ return jsonify(employees)
 
 if __name__ == '__main__':
     app.run(port=5000)
