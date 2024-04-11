f32c6abff29d5d7a342b033eef249c2537ca4a0c
skuo <skuo004@ucr.edu>
2024-04-10 17:19:35 -0700

    feat: add players.csv, read csv

--- src/app.py
+++ src/app.py
@@ -1,7 +1,18 @@
 import json
+import csv
+import os
 from flask import Flask, jsonify, request
 
 app = Flask(__name__)
+
+# TODO: separate thread/file
+# TODO: in memory db
+current_dir = os.path.dirname(os.path.abspath(__file__))
+datasrc_path = os.path.join(os.path.dirname(current_dir), 'players.csv')
+with open(datasrc_path, 'r', encoding='utf-8') as csvfile:
+   csv_reader = csv.reader(csvfile)
+   for row in csv_reader:
+      print(row)
 
 # TODO: read csv, serve players instead
 employees = [ { 'id': 1, 'name': 'Ashley' }, { 'id': 2, 'name': 'Kate' }, { 'id': 3, 'name': 'Joe' }]
