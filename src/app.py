ca1eaae67f6bb09054de062cbf78002768f9ab40
skuo <skuo004@ucr.edu>
2024-04-10 17:21:25 -0700

    feat: simple in-memory dictionary

--- src/app.py
+++ src/app.py
@@ -5,6 +5,8 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
+players = []
+
 # TODO: separate thread/file
 # TODO: in memory db
 current_dir = os.path.dirname(os.path.abspath(__file__))
@@ -12,14 +14,11 @@ datasrc_path = os.path.join(os.path.dirname(current_dir), 'players.csv')
 with open(datasrc_path, 'r', encoding='utf-8') as csvfile:
    csv_reader = csv.reader(csvfile)
    for row in csv_reader:
-      print(row)
-
-# TODO: read csv, serve players instead
-employees = [ { 'id': 1, 'name': 'Ashley' }, { 'id': 2, 'name': 'Kate' }, { 'id': 3, 'name': 'Joe' }]
+      players.append(row)
 
 @app.route('/api/players', methods=['GET'])
 def get_players():
- return jsonify(employees)
+ return jsonify(players)
 
 if __name__ == '__main__':
     app.run(port=5000)
